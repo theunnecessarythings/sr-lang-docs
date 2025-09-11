0083389e12441b7948dc7c5cad8996345281a43a
Sreeraj Ramachandran <sreerajr000@gmail.com>
2025-09-11 05:03:26 -0500

    site update

--- astro.config.ts
+++ astro.config.ts
@@ -25,7 +25,7 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // Top-Level Options
-  site: 'https://sreeraj.in',
+  site: 'https://sreeraj.in/language/',
   // Deploy to a sub path; See https://astro-pure.js.org/docs/setup/deployment#platform-with-base-path
   base: '/language/',
   trailingSlash: 'never',
